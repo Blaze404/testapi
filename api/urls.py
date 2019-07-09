9e6dd93e2cc875c4b0dead3927b3feb6588d39f9
Blaze404 <mus.qazi999@gmail.com>
2019-07-09 18:12:11 +0530

    add all urls

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,7 @@ app_name = 'api'
 
 urlpatterns = [
     path('patient/count', views.patient_count, name='patient_count'),
-    path('trend/<slug:start_date>/<slug:end_date>', views.age, name='trend'),
-    path('age/<slug:start_date>/<slug:end_date>', views.patient_flow, name='age'),
+    path('trend/<slug:start_date>/<slug:end_date>', views.trend, name='trend'),
+    path('age/<slug:start_date>/<slug:end_date>', views.age, name='age'),
+    path('patient_flow/<slug:start_date>/<slug:end_date>', views.patient_flow, name='age'),
 ]
